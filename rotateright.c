4258efa728fd5e3cab765e1d044147098d7518e7
nehasingh1611 <nera16cs@cmrit.ac.in>
2017-03-20 15:03:07 +0530

    rotateright.c

--- rotateright.c
+++ rotateright.c
@@ -1,17 +1,24 @@
 #include<stdio.h>
-int rotateright(int x,int n)
+unsigned int rotateright(unsigned int x,unsigned int n)
 {
-    n=n%(sizeof(int)*8);
-    int l=(sizeof(int)*8)-n;
-    return x>>n|x<<l;
+   for(int i=0;i<n;i++)
+   {    if(x%2==0)
+        {x>>1;
+        }
+       else
+       {x=x>>1;
+       x=x|(1<<(sizeof(int)*8)-1);
+       }
+   }
+   return x;
 }
-int main()
-{   int x,n;
+unsigned int main()
+{   unsigned int x,n;
     printf("enter the number\n");
     scanf("%d",&x);
     printf("enter the number of rotation\n");
     scanf("%d",&n);
-    int newnum=rotateright(x,n);
-    printf("the number after (%d) rotation is%d \n",n,newnum);
+    unsigned int newnum=rotateright(x,n);
+    printf("the number after (%d) rotation is%x \n",n,newnum);
     return 0;
 }
